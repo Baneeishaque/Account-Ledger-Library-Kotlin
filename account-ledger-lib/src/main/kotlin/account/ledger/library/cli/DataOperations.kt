6a01dc2fcd20803f35807b0bcd5af8fa610f27fe
Banee Ishaque K <Baneeishaque@gmail.com>
2023-03-16 12:58:28 +0530

    to-do

--- account-ledger-lib/src/main/kotlin/account/ledger/library/cli/DataOperations.kt
+++ account-ledger-lib/src/main/kotlin/account/ledger/library/cli/DataOperations.kt
@@ -7,6 +7,7 @@ import java.util.*
 fun getUserInitialTransactionDateFromUsername(username: String): LocalDate {
 
     return LocalDate.parse(
+        // TODO : Use environment variable for user prefix
         username.removePrefix("banee_ishaque_k_"),
         DateTimeFormatter.ofPattern("dd_MM_yyyy", Locale.getDefault())
     )
