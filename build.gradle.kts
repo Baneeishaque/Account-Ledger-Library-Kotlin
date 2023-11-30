790bc07ab97d361daf3e5f74f61b0c46e642a45e
Banee Ishaque K <baneeishaque@gmail.com>
2023-11-30 19:46:48 +0530

    Updated submodule common-lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     
     // TODO : Use BoM
     // val kotlinVersion = "1.9.20-mercury-653"
-    val kotlinVersion = "1.9.20-station-601"
+    val kotlinVersion = "2.0.0-Beta1-16"
     kotlin("jvm") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
 }
