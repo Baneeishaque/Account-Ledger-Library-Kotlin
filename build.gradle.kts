bb59b70d552b425b7931fdc0ada34ffc0b439d54
Banee Ishaque K <baneeishaque@gmail.com>
2023-11-30 19:55:47 +0530

    Clean

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     
     // TODO : Use BoM
-    // val kotlinVersion = "1.9.20-mercury-653"
     val kotlinVersion = "2.0.0-Beta1-16"
     kotlin("jvm") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
