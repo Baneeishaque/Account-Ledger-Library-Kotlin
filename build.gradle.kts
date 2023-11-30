dd66558b4bc72b8e042507390d93f76f40bf18bd
Banee Ishaque K <baneeishaque@gmail.com>
2023-11-30 20:17:06 +0530

    plugin for sub project fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,6 @@ plugins {
     // TODO : Use BoM
     val kotlinVersion = "2.0.0-Beta1-16"
     kotlin("jvm") version kotlinVersion apply false
+    kotlin("multiplatform") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
 }
