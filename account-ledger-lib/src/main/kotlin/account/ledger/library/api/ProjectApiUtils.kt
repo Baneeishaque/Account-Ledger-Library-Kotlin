7607aaf8d5be0e10fb6ed7fe2bf5c8a184f0bf67
Banee Ishaque K <baneeishaque@gmail.com>
2023-12-01 13:28:09 +0530

    getServerApiMethodSelectUserAccountsFullUrlForUser

--- account-ledger-lib/src/main/kotlin/account/ledger/library/api/ProjectApiUtils.kt
+++ account-ledger-lib/src/main/kotlin/account/ledger/library/api/ProjectApiUtils.kt
@@ -2,13 +2,32 @@ package account.ledger.library.api
 
 import common.utils.library.utils.ApiUtilsCommon
 
-internal object ProjectApiUtils {
+object ProjectApiUtils {
 
-    internal fun getServerApiMethodAbsoluteUrl(serverApiMethodName: String): String {
+    @JvmStatic
+    fun getServerApiMethodAbsoluteUrl(serverApiMethodName: String): String {
 
         return ApiUtilsCommon.getServerApiMethodAbsoluteUrl(
             serverApiMethodName = serverApiMethodName,
             serverFileExtension = ApiConstants.serverFileExtension
         )
+    }
+
+    @JvmStatic
+    fun getServerApiMethodFullUrl(serverApiMethodName: String): String {
+
+        return "${ApiConstants.serverApiAddress}${getServerApiMethodAbsoluteUrl(serverApiMethodName = serverApiMethodName)}"
+    }
+
+    @JvmStatic
+    fun getServerApiMethodSelectUserAccountsFullUrl(): String {
+
+        return getServerApiMethodFullUrl(serverApiMethodName = ApiConstants.selectUserAccountsFullMethod)
+    }
+
+    @JvmStatic
+    fun getServerApiMethodSelectUserAccountsFullUrlForUser(userId: UInt): String {
+
+        return "${getServerApiMethodSelectUserAccountsFullUrl()}?user_id=$userId"
     }
 }
